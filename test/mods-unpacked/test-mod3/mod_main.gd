2753dc77ae15ff579097566166680e8a5f412e2d
Kai <kai@kana.jetzt>
2024-11-15 13:29:30 +0100

    test: :test_tube: Ported tests to 4.x

---
+++ test/mods-unpacked/test-mod3/mod_main.gd
@@ -0,0 +1,31 @@
+extends Node
+
+
+const TEST_MOD3_DIR := "test-mod3"
+const TEST_MOD3_LOG_NAME := "test-mod3:Main"
+
+var mod_dir_path := ""
+var extensions_dir_path := ""
+var translations_dir_path := ""
+
+
+func _init() -> void:
+	mod_dir_path = ModLoaderMod.get_unpacked_dir().path_join(TEST_MOD3_DIR)
+	# Add extensions
+	install_script_extensions()
+	# Add translations
+	add_translations()
+
+
+func install_script_extensions() -> void:
+	extensions_dir_path = mod_dir_path.path_join("extensions")
+	ModLoaderMod.install_script_extension(extensions_dir_path.path_join("script_extension_sorting/script_b.gd"))
+	ModLoaderMod.install_script_extension(extensions_dir_path.path_join("script_extension_sorting/script_d.gd"))
+
+
+func add_translations() -> void:
+	translations_dir_path = mod_dir_path.path_join("translations")
+
+
+func _ready() -> void:
+	pass
